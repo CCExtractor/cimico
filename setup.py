5368cde76de7d4f78f1e84df72f507c5301fcf8b
knightron0 <sarthak.robo@gmail.com>
2020-01-20 10:43:21 +0530

    Update to v1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cimico", 
-    version="1.0.3",
+    version="1.0.4",
     author="knightron0",
     entry_points = {
         'console_scripts': ['cimico = cimico.main:main'],
